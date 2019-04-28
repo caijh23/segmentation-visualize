c77289536b219f6f391e232ce20bb6caa721edb6
caijjh <caijh23@gmail.com>
2019-04-28 23:42:01 +0800

    add get /templates for test front end

--- mock-server/app.js
+++ mock-server/app.js
@@ -16,12 +16,23 @@ app.use(bodyParser());
 router.post('/inputs', async (ctx, next) => {
   const json = {
     imgId: 1
-  }
+  };
   ctx.set("Content-Type", "application/json");
-  ctx.response.body = JSON.stringify(json)
+  ctx.response.body = JSON.stringify(json);
 });
+
+router.get('/templates', async (ctx, next) => {
+  const json = {
+    template_lists: [
+      {text: '假装有模型', templateId: 1},
+      {text: '测试模型二', templateId: 2}
+    ]
+  };
+  ctx.set("Content-Type", "application/json");
+  ctx.response.body = JSON.stringify(json);
+})
 
 app.use(router.routes());
 
 app.listen(8000);
-console.log('app started at port 8000...');
+console.log('🌎\t\b\b\b\b\bListening : app started at port 8000...')
