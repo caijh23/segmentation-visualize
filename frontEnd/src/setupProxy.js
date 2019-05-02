f7180fc76c0d0b978e3fce8603d70156ae3f62ef
caijjh <caijh23@gmail.com>
2019-05-02 14:53:18 +0800

    一些细节的小改动

--- frontEnd/src/setupProxy.js
+++ frontEnd/src/setupProxy.js
@@ -1,9 +1,12 @@
-const proxy = require('http-proxy-middleware');
-module.exports = function (app) {
-  app.use(proxy('/api', {
-    target: 'http://localhost:8000/',
-    pathRewrite: {
-      '^/api': '/'
-    }
-  }));
-};
+const proxy = require('http-proxy-middleware')
+
+module.exports = function(app) {
+  app.use(
+    proxy('/api', {
+      target: 'http://localhost:8000/',
+      pathRewrite:{
+        '^/api': '/'
+      },
+      ws: true
+  }))
+}
